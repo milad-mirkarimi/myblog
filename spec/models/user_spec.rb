489986843ad4f3d1f376047ea0bd97c24564b41b
theGrumpContainment <milad.mirkarimi@aplyid.com>
2024-05-19 19:22:34 +1000

    init commit

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with valid attributes" do
+    user = User.new(name: "John Doe", email: "john@example.com")
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a name" do
+    user = User.new(name: nil, email: "john@example.com")
+    expect(user).not_to be_valid
+  end
+end
